46923ffe02e942d9c5ffca8b5d177a65afdb7a33
user13356 <u118411471@byui.edu>
2026-09-13 02:00:54 +0200

    Week 1 Work

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
 
-router.get('/', (reg, res) => { res.send('Hello World'); });
+router.get('/', (req, res) => { res.send('Hello World'); });
 
 module.exports = router;
